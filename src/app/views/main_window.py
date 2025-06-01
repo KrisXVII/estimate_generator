9a214e8031d4c160689d87f06fb8510f38645083
Cristian Radulescu <kristianradulescu@gmail.com>
2025-06-01 14:55:50 +0200

    feat: added button to download doc

--- src/app/views/main_window.py
+++ src/app/views/main_window.py
@@ -1,19 +1,26 @@
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+
+
+def generate_estimate():
+    print("palle")
 
 
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        layout = QVBoxLayout(central_widget)
 
         self.setWindowTitle("Genera preventivo")
 
-        label = QLabel("Generatore preventivo")
-        label.setAlignment(Qt.AlignCenter)
+        btn = QPushButton("Genera Preventivo")
+        btn.clicked.connect(generate_estimate)
+        btn.setFixedHeight(100)
+        layout.addWidget(btn)
 
-        self.setCentralWidget(label)
-        self.setFixedSize(QSize(400, 300))
+        self.setFixedSize(QSize(500, 400))
 
 
-def generate_estimate():
-    print("let's do it")
