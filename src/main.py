74c858047606ddb0312f1aab7abaf9998155516e
Cristian Radulescu <kristianradulescu@gmail.com>
2025-06-01 11:30:56 +0200

    chore: added command line args for loop iterator

--- src/main.py
+++ src/main.py
@@ -2,12 +2,12 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from src.app.views.main_window import MainWindow
-import sys
+import sys # for command line arguments
 
 
-app = QApplication(sys.argv)
+app = QApplication(sys.argv) # application instance, pass [] if won't be using command line arguments
 
 window = MainWindow()
-window.show()
+window.show() # windows are hidden by default
 
 app.exec_()
