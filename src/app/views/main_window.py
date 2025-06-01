0e76d08b838d209eb56496483e46dc0a3b00033a
Cristian Radulescu <kristianradulescu@gmail.com>
2025-06-01 16:14:56 +0200

    feat: document generation

--- src/app/views/main_window.py
+++ src/app/views/main_window.py
@@ -1,11 +1,52 @@
-from PyQt5 import QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-
+from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Inches, Pt
+from datetime import date
+from .const import COMPANY, ADDRESS, CITY, TAX_CODE, VAT_ID
 
 def generate_estimate():
-    print("palle")
+    document = Document()
+    header = document.sections[0].header
+    document.sections[0].header_distance = Inches(1.5)
+    table = header.add_table(rows=1, cols=3, width=Inches(6.5))  # Full page width
+    table.autofit = False
 
+    # col1
+    left_col = table.cell(0, 0)
+    left_col.width = Inches(2.5)
+    left_para = left_col.paragraphs[0]
+    left_para.add_run("Informazioni cliente")
+
+    left_col.paragraphs[0].paragraph_format.border_bottom = True
+    left_col.paragraphs[0].paragraph_format.border_top = True
+    left_col.paragraphs[0].paragraph_format.border_left = True
+    left_col.paragraphs[0].paragraph_format.border_right = True
+
+    # col2
+    middle_col = table.cell(0, 1)
+    middle_col.width = Inches(1.5)
+    middle_para = middle_col.paragraphs[0]
+    middle_para.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    middle_para.add_run("\n\n\n")
+    middle_para.add_run("Preventivo").bold = True
+
+    # col3
+    right_col = table.cell(0, 2)
+    right_col.width = Inches(2.5)
+    right_para = right_col.paragraphs[0]
+    right_para.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+
+    right_para.add_run(COMPANY + "\n")
+    right_para.add_run(ADDRESS + "\n")
+    right_para.add_run(CITY + "\n")
+    right_para.add_run(TAX_CODE + "\n")
+    right_para.add_run(VAT_ID + "\n")
+
+    date_sig = date.today().strftime('%d-%m-%Y')
+    document.add_paragraph("Main document content goes here...")
+    document.save("Preventivo_{date}.docx".format(date=date_sig))
 
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
@@ -22,5 +63,3 @@ class MainWindow(QMainWindow):
         layout.addWidget(btn)
 
         self.setFixedSize(QSize(500, 400))
-
-
